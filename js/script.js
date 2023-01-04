a19cd9b26c38a7684b4ba857b919d883a90714f5
Javad Shahnavaz <javad.shahnavaz@gmail.com>
2023-01-04 03:51:06 +0330

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,40 @@
+﻿let $ = document
+let locationKey = $.querySelector('#location')
+let whichKey = $.querySelector('#which')
+let codeKey = $.querySelector('#code')
+let keyKey = $.querySelector('#key')
+let title = $.querySelector('title')
+let keyCodeKey = $.querySelector('#keyCode')
+
+
+
+document.body.addEventListener('keydown', function (event) {
+
+	event.preventDefault()
+	starter.style.display = 'none'
+	heading.style.display = 'flex'
+	ascii.style.display = 'flex'
+	infos.style.display = 'flex'
+
+	let eventLocation =  event.location
+	locationKey.innerHTML = eventLocation
+
+	let eventkeyCodeKey =  event.key
+	keyCodeKey.innerHTML = eventkeyCodeKey
+
+	let eventkey =  event.key
+	keyKey.innerHTML = eventkey
+
+	let eventCode =  event.code
+	codeKey.innerHTML = eventCode
+
+	let eventKeyCode =  event.keyCode
+	whichKey.innerHTML = eventKeyCode
+
+})
+
+
+
+
+
+// JS Code By Javad Shahnavaz
